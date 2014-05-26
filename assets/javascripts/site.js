b38827ac67900506cb9761c2d6f1de148e2b7eb6
eddie cianci <defeated2k4@gmail.com>
2014-05-26 12:04:16 -0400

    display hash in address bar after smooth scroll

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -1,13 +1,17 @@
 jQuery(function($){
   $('.background').parallax();
 
-  $('.nav a').click(function(e){
-    var $el = $(this.hash);
+  $('.navbar').on('click', 'a', function(e){
+    var hash = this.hash;
+    var $el = $(hash);
     if($el.length) {
       e.preventDefault();
       $.scrollTo($el, {
         axis:     'y',
-        duration: 500
+        duration: 500,
+        onAfter:  function(){
+          document.location.hash = hash;
+        }
       });
     }
   });
